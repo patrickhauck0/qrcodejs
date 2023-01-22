21675322c5ee23c2c0783c6ed62264738bde6b4c
Patrick Hauck <patrickhauck0@gmail.com>
2023-01-22 16:02:33 -0300

    Criado a estilização do site e retoques finais

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 function GerarQRCode() {
     var inputUsuario = document.querySelector('textarea').value;
-    var GoogleChartAPI = 'https://chart.googleapis.com/chart?cht=qr&chs=500x500&chld=H&chl=';
+    var GoogleChartAPI = 'https://chart.googleapis.com/chart?cht=qr&chs=400x400&chld=H&chl=';
     var conteudoQRCode = GoogleChartAPI + encodeURIComponent(inputUsuario);
     document.querySelector('#QRCodeIMG').src = conteudoQRCode;
 }
